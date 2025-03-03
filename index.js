d912c17f02f5cbbad7489af887d7950957640c4b
skebila <stevenkebila@gmail.com>
2025-03-03 16:31:01 -0500

    Add test API endpoint to verify server functionality

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
   res.send("Numio Backend is running!");
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
